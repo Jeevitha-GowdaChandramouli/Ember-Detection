4e7436d2950d4d7c69a97a96b86cc10e53f80854
Jeevitha Gowda Chandramouli <gowdacha@usc.edu>
2022-04-27 00:47:14 -0500

    Add files via upload

---
+++ config/faster_rcnn_r50_caffe_fpn_mstrain_1x_ember.py
@@ -0,0 +1,51 @@
+# The new config inherits a base config to highlight the necessary modification
+_base_ = '../faster_rcnn/faster_rcnn_r50_caffe_fpn_mstrain_1x_coco.py'
+
+
+# We also need to change the num_classes in head to match the dataset's annotation
+# custom_imports = dict(
+#     imports=['mmdet.models.backbones.resnet_mod.py'],
+#     allow_failed_imports=False)
+
+model = dict(
+    backbone=dict(
+        type = 'ResNet_mod',
+        depth=18,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=False),
+        norm_eval=True,
+        style='caffe',
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint='open-mmlab://detectron2/resnet50_caffe')),
+    roi_head=dict(
+        bbox_head=dict(num_classes=1),))
+
+# Modify dataset related settings
+dataset_type = 'COCODataset'
+classes = ('__background__', 'ember')
+data = dict(
+    train=dict(
+        img_prefix='data/EmberDetection/train2022/',
+        classes=classes,
+        ann_file='data/EmberDetection/annotations/ember_train_dataset.json'),
+    val=dict(
+        img_prefix='data/EmberDetection/val2022/',
+        classes=classes,
+        ann_file='data/EmberDetection/annotations/ember_val_dataset.json'),
+    test=dict(
+        img_prefix='data/EmberDetection/val2022/',
+        classes=classes,
+        ann_file='data/EmberDetection/annotations/ember_val_dataset.json'))
+
+# We can use the pre-trained Mask RCNN model to obtain higher performance
+evaluation = dict(interval=12, metric='bbox')
+optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
+
+checkpoint_config = dict(interval=1, by_epoch=False)
+
+load_from = 'checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+work_dir = './exps_filtersize_mod'
+gpu_ids = [1]
---
+++ config/faster_rcnn_r50_caffe_fpn_mstrain_1x_ember_RPN.py
@@ -0,0 +1,57 @@
+# The new config inherits a base config to highlight the necessary modification
+_base_ = '../faster_rcnn/faster_rcnn_r50_caffe_fpn_mstrain_1x_coco.py'
+
+model = dict(
+    backbone=dict(
+        type = 'ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=False),
+        norm_eval=True,
+        style='caffe',
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint='open-mmlab://detectron2/resnet50_caffe')),
+    rpn_head=dict(
+        type='RPNHead',
+        in_channels=256,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            scales = [4],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64]),
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+    roi_head=dict(
+        bbox_head=dict(num_classes=1),))
+
+# Modify dataset related settings
+dataset_type = 'COCODataset'
+classes = ('ember',)
+data = dict(
+    train=dict(
+        img_prefix='data/0415_take2/train/',
+        classes=classes,
+        ann_file='data/0415_take2/annotations/ember_train_dataset.json'),
+    val=dict(
+        img_prefix='data/0415_take2/val/',
+        classes=classes,
+        ann_file='data/0415_take2/annotations/ember_val_dataset.json'),
+    test=dict(
+        img_prefix='data/0415_take2/val/',
+        classes=classes,
+        ann_file='data/0415_take2/annotations/ember_val_dataset.json'))
+
+# We can use the pre-trained Mask RCNN model to obtain higher performance
+evaluation = dict(interval=12, metric='bbox')
+optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
+
+checkpoint_config = dict(interval=1, by_epoch=False)
+
+load_from = 'checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+work_dir = './exps/0415_take2_onlylowscale'
+gpu_ids = [0]
