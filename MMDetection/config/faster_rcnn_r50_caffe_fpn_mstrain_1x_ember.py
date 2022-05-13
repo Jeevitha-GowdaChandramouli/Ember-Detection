a50136ea2c6214fc0c2aba7c9512cdfea2eebb56
Jeevitha Gowda Chandramouli <gowdacha@usc.edu>
2022-05-12 22:46:25 -0500

    Update faster_rcnn_r50_caffe_fpn_mstrain_1x_ember.py

--- MMDetection/config/faster_rcnn_r50_caffe_fpn_mstrain_1x_ember.py
+++ MMDetection/config/faster_rcnn_r50_caffe_fpn_mstrain_1x_ember.py
@@ -5,7 +5,7 @@ model = dict(
     backbone=dict(
         type = 'ResNet_mod',
         depth=18,
-        num_stages=4,
+        num_stages=4,,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=False),
